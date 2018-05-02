9ecc03bfff10f86e4c1540a66873eb466e458f31
justinienghorra <justinienghorra@hotmail.fr>
2018-05-02 16:25:54 +0200

    model added

--- src/Algo.java
+++ src/Algo.java
@@ -1,13 +1,13 @@
 
 public class Algo {
 
-	protected ModelValues[][] modele;
+	protected ModelValues[][] model;
 	
 	public Algo() {
-		modele = new ModelValues[4][4]; 
+		model = new ModelValues[4][4]; 
 		for (int i = 0; i < 4; i=i+1) {
 			for (int j = 0; j < 4; j=j+1) {
-				modele[i][j] = ModelValues.NotVisited;
+				model[i][j] = ModelValues.NotVisited;
 			}
 		}
 		
